c4374134df3b99a0c6586e89c891c1833a510c73
parthpatel1234-dev <parthpatel1234000@gmail.com>
2025-07-26 12:31:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
     const apiUrl = await getApiUrl("");
     await axios.get(apiUrl).then((response) => {
-      document.getElementById("message").innerHTML = response?.data?.message;
+      document.getElementById("message").innerHTML = response?.data;
     }).catch((error) => {
       console.error("Error fetching users:", error);
     });
