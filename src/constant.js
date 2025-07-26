7cb271dcaf64cd9b8400f45d12e9ee59fb80a6b7
parthpatel1234-dev <parthpatel1234000@gmail.com>
2025-07-26 12:20:13 +0530

    Update end point dev

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 // export const endpoint = 'http://15.206.164.156:8080';
-export const endpoint = 'https://743gyaarbc.execute-api.ap-south-1.amazonaws.com';
+export const endpoint = 'https://9sgj1yaexh.execute-api.ap-south-1.amazonaws.com';
 
 export const API_PATH = '/api/users';
